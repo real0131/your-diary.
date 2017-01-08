ab025cf93013a3358e99539fafb658a030c033f4
Jaeho Cho <real0131@gmail.com>
2017-01-09 01:17:27 +0900

    add modal

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ $('.segmented-cell').click(function(){
     $(this).attr('selected','selected');
   }
 });
+
+$('.diary-card').click(function(){
+  $('body').on('scroll touchmove mousewheel', function(e) {
+     e.preventDefault();
+     e.stopPropagation();
+     return false;
+  });
+  $('.modal-container').removeAttr('display');
+});
+
+$('.modal-cancel').click(function() {
+  $('body').off('scroll touchmove mousewheel');
+  $('.modal-container').attr('display','none');
+});
