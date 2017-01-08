b19838417aed52f50020964cda971030d051c87f
Jaeho Cho <real0131@gmail.com>
2017-01-09 03:33:40 +0900

    update android back button event

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//segmented-cell click event (change page-view)
 $('.segmented-cell').click(function(){
   if(!($(this).attr('selected')=='selected')){
     $(".segmented-cell[selected='selected']").removeAttr('selected');
@@ -5,6 +6,7 @@ $('.segmented-cell').click(function(){
   }
 });
 
+//diary-card click event (open modal)
 $('.diary-card').click(function(){
   $('body').on('scroll touchmove mousewheel', function(e) {
      e.preventDefault();
@@ -14,7 +16,14 @@ $('.diary-card').click(function(){
   $('.modal-container').removeAttr('display');
 });
 
+//cancel button(in modal) click event (close modal)
 $('.modal-cancel').click(function() {
+  $('body').off('scroll touchmove mousewheel');
+  $('.modal-container').attr('display','none');
+});
+
+//android back button event (close modal)
+$('document').bind('keydown',function(event){
   $('body').off('scroll touchmove mousewheel');
   $('.modal-container').attr('display','none');
 });
