2a54d7e4dd5ea815f6d2aef5c19facfb1579e91e
Jaeho Cho <real0131@gmail.com>
2017-01-16 04:03:42 +0900

    add calendar and block scroll at calendar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var calendarIndex = 2;
+
 //segmented-cell click event (change page-view)
 $('.segmented-cell').click(function(){
   if(!($(this).attr('selected')=='selected')){
@@ -9,39 +11,53 @@ $('.segmented-cell').click(function(){
 
 //segmented-control paging
 function segmented_paging(target){
+  //TODO:add animation
   $(".page-view[display='show']").removeAttr('display');
   $('.page-view:nth-child('+(target.index()+1)+')').attr('display','show');
+  if ((target.index()+1)==calendarIndex) {
+    stopScroll();
+  }else {
+    startScroll();
+  }
 }
 
-//diary-card click event (open modal)
-$('.diary-card').click(function(){
+function stopScroll(){
   $('body').on('scroll touchmove mousewheel', function(e) {
      e.preventDefault();
      e.stopPropagation();
      return false;
   });
+}
+
+function startScroll(){
+  $('body').off('scroll touchmove mousewheel');
+}
+
+//diary-card click event (open modal)
+$('.diary-card').click(function(){
+  stopScroll();
   $('.modal-container').removeAttr('display');
 });
 
 //cancel button(in modal) click event (close modal)
 $('.modal-cancel').click(function() {
-  $('body').off('scroll touchmove mousewheel');
+  startScroll();
   $('.modal-container').attr('display','none');
 });
 
-//android back button event (close modal)
+//get diary-card Entries
+$('.tool-entries').html($('.diary-card').length+" Entries");
+
+/*//android back button event (close modal)
 $('document').bind('keydown',function(event){
   if (event.keyCode == 27) {
     $('body').off('scroll touchmove mousewheel');
     $('.modal-container').attr('display','none');
     event.preventDefault();
   }
-});
+});*/
 
-//get diary-card Entries
-$('.tool-entries').html($('.diary-card').length+" Entries");
-
-//swipe function
+/*//swipe function
 $('.page-view').on('swiperight',function(){
   if ($(this) == $('.page-view:nth-child(1)')) {
     //not paging
@@ -54,4 +70,14 @@ $('page-view').on('swipeleft',function(){
     //not paging
   }
     //지금의 nht-child값을 가지고와서 +1
-});
+});*/
+
+//set calendar date(to today)
+function setCalendar(){
+
+}
+
+//set color theme (taki: #67b5e6, mitsuha: #)
+function setColorTheme(){
+
+}
